b5a83cfdcaa72002857d5ef3871b33043520753a
Yasir Oladimeji <yasiroyebo@gmail.com>
2025-02-04 12:19:16 +0100

    Add project files.

---
+++ HNG_Tasks/Controller/InfoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNG_Tasks.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok(new
+            {
+                email = "yasiroyebo@gmail.com",
+                currentDateTime = DateTime.UtcNow.ToString("o"),
+                githubUrl = "https://github.com/YastecHub/HNG_Tasks"
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# HNG12 Stage 0 - Public API
+
+This is a simple public API developed for the HNG12 Internship Stage 0 task.
+
+## 🚀 API Endpoint
+Base URL: `https://hng-task.onrender.com`
+
+### 📌 `GET /api/info`
+This endpoint returns basic information about the developer and the project.
+
+**Response:**
+```json
+{
+  "email": "yasiroyebo@gmail.com",
+  "current_datetime": "2025-02-03T16:17:18.8736560Z",
+  "github_url": "https://github.com/YastecHub/HNG_Tasks"
+}
