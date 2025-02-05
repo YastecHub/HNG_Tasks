e952ce5eebe25ab20e68ac4ac91449dd9e20d347
Yasir Oladimeji <yasiroyebo@gmail.com>
2025-02-05 14:25:56 +0100

    Updates

--- HNG_Tasks/Controller/NumberClassificationController.cs
+++ HNG_Tasks/Controller/NumberClassificationController.cs
@@ -1,6 +1,135 @@
 ﻿using HNG_Tasks.Responses;
 using Microsoft.AspNetCore.Mvc;
 
+//namespace HNG_Tasks.Controller
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class NumberClassificationController : ControllerBase
+//    {
+//        private readonly IHttpClientFactory _httpClientFactory;
+
+//        public NumberClassificationController(IHttpClientFactory httpClientFactory)
+//        {
+//            _httpClientFactory = httpClientFactory;
+//        }
+
+//        [HttpGet("classify-number")]
+//        public async Task<IActionResult> ClassifyNumber([FromQuery] string number)
+//        {
+//            if (string.IsNullOrEmpty(number))
+//            {
+//                return BadRequest(new
+//                {
+//                    error = true,
+//                    number = ""
+//                });
+//            }
+//            if (!int.TryParse(number, out int parsedNumber))
+//            {
+//                return BadRequest(new
+//                {
+//                    number = number,
+//                    error = true,
+//                });
+//            }
+
+//            var response = new NumberClassificationResponse
+//            {
+//                Number = parsedNumber,
+//                is_prime = IsPrime(parsedNumber),
+//                is_perfect = IsPerfect(parsedNumber),
+//                Properties = GetProperties(parsedNumber),
+//                digit_sum = GetDigitSum(parsedNumber),
+//                fun_fact = await GetFunFact(parsedNumber)
+//            };
+
+//            return Ok(response);
+//        }
+
+//        private bool IsPrime(int number)
+//        {
+//            if (number < 2)
+//            {
+//                return false;
+//            }
+//            for (int i = 2; i <= Math.Sqrt(number); i++)
+//            {
+//                if (number % i == 0)
+//                {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+
+//        private bool IsPerfect(int number)
+//        {
+//            int sum = 0;
+//            for (int i = 1; i <= number / 2; i++)
+//            {
+//                if (number % i == 0)
+//                {
+//                    sum += i;
+//                }
+//            }
+//            return sum == number;
+//        }
+
+//        private List<string> GetProperties(int number)
+//        {
+//            var properties = new List<string>();
+
+//            if (IsArmstrong(number))
+//            {
+//                properties.Add("armstrong");
+//            }
+//            if (number % 2 == 0)
+//            {
+//                properties.Add("even");
+//            }
+//            else
+//            {
+//                properties.Add("odd");
+//            }
+//            return properties;
+//        }
+
+//        private bool IsArmstrong(int number)
+//        {
+//            if (number < 0)
+//            {
+//                return false;
+//            }
+//            int sum = 0;
+//            int temp = number;
+//            int digits = number.ToString().Length;
+
+//            while (temp != 0)
+//            {
+//                int digit = temp % 10;
+//                sum += (int)Math.Pow(digit, digits);
+//                temp /= 10;
+//            }
+//            return sum == number;
+//        }
+
+//        private int GetDigitSum(int number)
+//        {
+//            return Math.Abs(number).ToString().Sum(c => c - '0');
+//        }
+
+//        private async Task<string> GetFunFact(int number)
+//        {
+//            var client = _httpClientFactory.CreateClient();
+//            return await client.GetStringAsync($"http://numbersapi.com/{number}/math");
+//        }
+//    }
+//}
+
+
+using System.Collections.Concurrent;
+
 namespace HNG_Tasks.Controller
 {
     [Route("api/[controller]")]
@@ -8,6 +137,7 @@ namespace HNG_Tasks.Controller
     public class NumberClassificationController : ControllerBase
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private static readonly ConcurrentDictionary<int, string> _funFactCache = new();
 
         public NumberClassificationController(IHttpClientFactory httpClientFactory)
         {
@@ -15,96 +145,78 @@ namespace HNG_Tasks.Controller
         }
 
         [HttpGet("classify-number")]
-        public async Task<IActionResult> ClassifyNumber([FromQuery] string number)
+        public async Task<IActionResult> ClassifyNumber()
         {
-            if (string.IsNullOrEmpty(number))
+            var numberQuery = HttpContext.Request.Query["number"].ToString();
+
+            // ✅ Custom validation: Empty number parameter
+            if (string.IsNullOrWhiteSpace(numberQuery))
             {
-                return BadRequest(new
-                {
-                    error = true,
-                    message = "Number is required"
-                });
-            }
-            if (!int.TryParse(number, out int parsedNumber))
-            {
-                return BadRequest(new
-                {
-                    number = number,
-                    error = true,
-                });
+                return BadRequest(new { error = true, number = "" });
             }
 
-            var response = new NumberClassificationResponse
+            if (!int.TryParse(numberQuery, out int parsedNumber))
+            {
+                return BadRequest(new { error = true, number = numberQuery });
+            }
+
+            var isPrimeTask = Task.Run(() => IsPrime(parsedNumber));
+            var isPerfectTask = Task.Run(() => IsPerfect(parsedNumber));
+            var isArmstrongTask = Task.Run(() => IsArmstrong(parsedNumber));
+            var digitSumTask = Task.Run(() => GetDigitSum(parsedNumber));
+            var funFactTask = GetFunFact(parsedNumber);
+
+            await Task.WhenAll(isPrimeTask, isPerfectTask, isArmstrongTask, digitSumTask, funFactTask);
+
+            var properties = new List<string>();
+            if (isArmstrongTask.Result) properties.Add("armstrong");
+            properties.Add(parsedNumber % 2 == 0 ? "even" : "odd");
+
+            return Ok(new NumberClassificationResponse
             {
                 Number = parsedNumber,
-                is_prime = IsPrime(parsedNumber),
-                is_perfect = IsPerfect(parsedNumber),
-                Properties = GetProperties(parsedNumber),
-                digit_sum = GetDigitSum(parsedNumber),
-                fun_fact = await GetFunFact(parsedNumber)
-            };
-
-            return Ok(response);
+                is_prime = isPrimeTask.Result,
+                is_perfect = isPerfectTask.Result,
+                Properties = properties,
+                digit_sum = digitSumTask.Result,
+                fun_fact = funFactTask.Result
+            });
         }
 
         private bool IsPrime(int number)
         {
-            if (number < 2)
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int limit = (int)Math.Sqrt(number);
+            for (int i = 3; i <= limit; i += 2)
             {
-                return false;
-            }
-            for (int i = 2; i <= Math.Sqrt(number); i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
+                if (number % i == 0) return false;
             }
             return true;
         }
 
         private bool IsPerfect(int number)
         {
-            int sum = 0;
-            for (int i = 1; i <= number / 2; i++)
+            if (number < 1) return false;
+
+            int sum = 1, sqrt = (int)Math.Sqrt(number);
+            for (int i = 2; i <= sqrt; i++)
             {
                 if (number % i == 0)
                 {
-                    sum += i;
+                    sum += i + (i != number / i ? number / i : 0);
                 }
             }
-            return sum == number;
-        }
-
-        private List<string> GetProperties(int number)
-        {
-            var properties = new List<string>();
-
-            if (IsArmstrong(number))
-            {
-                properties.Add("armstrong");
-            }
-            if (number % 2 == 0)
-            {
-                properties.Add("even");
-            }
-            else
-            {
-                properties.Add("odd");
-            }
-            return properties;
+            return sum == number && number != 1;
         }
 
         private bool IsArmstrong(int number)
         {
-            if (number < 0)
-            {
-                return false;
-            }
-            int sum = 0;
-            int temp = number;
-            int digits = number.ToString().Length;
+            if (number < 0) return false;
 
+            int sum = 0, temp = number, digits = number.ToString().Length;
             while (temp != 0)
             {
                 int digit = temp % 10;
@@ -114,16 +226,20 @@ namespace HNG_Tasks.Controller
             return sum == number;
         }
 
-        private int GetDigitSum(int number)
-        {
-            return Math.Abs(number).ToString().Sum(c => c - '0');
-        }
+        private int GetDigitSum(int number) => Math.Abs(number).ToString().Sum(c => c - '0');
 
         private async Task<string> GetFunFact(int number)
         {
+            if (_funFactCache.TryGetValue(number, out string cachedFact))
+            {
+                return cachedFact;
+            }
+
             var client = _httpClientFactory.CreateClient();
-            return await client.GetStringAsync($"http://numbersapi.com/{number}/math");
+            string fact = await client.GetStringAsync($"http://numbersapi.com/{number}/math");
+
+            _funFactCache[number] = fact; // Cache response
+            return fact;
         }
     }
 }
-
